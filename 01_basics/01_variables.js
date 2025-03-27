83166947239325032badddef80397ef08f3bc095
Saim Ansari <92514011+Itssaimansari@users.noreply.github.com>
2025-03-27 12:35:45 +0000

    done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const accountId = 14444
 let accountEmail = "123@example.com"
 var accountPassword = "123456"
-
+let accountState;//undefined
 accountCity = "Kanpur"
 
-console.table([accountId,accountEmail,accountPassword,accountCity]);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
