72070d62007252a4bdc8b5face301404373b2b9e
Saim Ansari <92514011+Itssaimansari@users.noreply.github.com>
2025-03-27 12:54:10 +0000

    done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,7 @@ const accountId = 14444
 let accountEmail = "123@example.com"
 var accountPassword = "123456"
 let accountState;//undefined
-accountCity = "Kanpur"
+accountCity = "Kanpur"  
 
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+console.log("Done");
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,5 @@
+"use strict"// treats all JS code as a newer version
+
+//alert(3+3) // we are using nodeJs , not browser
+
+console.log(3+3) ; console.log("Saim"); //use new line and spaces for the code redability
