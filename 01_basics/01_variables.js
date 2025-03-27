0943c80c30ac54ff8e40da4806025ec4cad4aaa6
Saim Ansari <92514011+Itssaimansari@users.noreply.github.com>
2025-03-27 12:32:46 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 14444
+let accountEmail = "123@example.com"
+var accountPassword = "123456"
+
+accountCity = "Kanpur"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
